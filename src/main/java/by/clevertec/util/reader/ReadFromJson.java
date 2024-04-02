1c8a1ff699433e2db4c57556334c78acad807a5e
yury-1980 <58135208+yury-1980@users.noreply.github.com>
2024-04-02 13:28:42 +0300

    fix:tasks1-22 (#2)

---
+++ src/main/java/by/clevertec/util/reader/ReadFromJson.java
@@ -0,0 +1,38 @@
+package by.clevertec.util.reader;
+
+import by.clevertec.exception.IOFileException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.Locale;
+
+public class ReadFromJson implements Reader {
+
+    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
+
+    @Override
+    public <T> List<T> getModelData(String fileName, TypeReference<List<T>> typeReference) {
+        try {
+            return newMapper().readValue(new File(fileName), typeReference);
+
+        } catch (IOException e) {
+            throw new IOFileException(e.getMessage());
+        }
+    }
+
+    private static ObjectMapper newMapper() {
+        final ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        mapper.setDateFormat(DATE_FORMAT);
+        mapper.setLocale(Locale.ENGLISH);
+        mapper.registerModule(new JavaTimeModule());
+
+        return mapper;
+    }
+}
